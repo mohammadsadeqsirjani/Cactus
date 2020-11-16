a699f0e7f74afaa66f955ef14b22d4d47f95a01e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-16 18:56:49 +0330

    System.Boolean

--- Core/System.Boolean/Boolean.ToBinary.cs
+++ Core/System.Boolean/Boolean.ToBinary.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 
-namespace Core.System.Boolean
+public static partial class Extension
 {
-    public static partial class Extension
+    /// <summary>
+    ///     A bool extension method that convert this object into a binary representation.
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <returns>A binary representation of this object.</returns>
+    public static byte ToBinary(this bool @this)
     {
-        /// <summary>
-        ///     A bool extension method that convert this object into a binary representation.
-        /// </summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <returns>A binary representation of this object.</returns>
-        public static byte ToBinary(this bool @this)
-        {
-            return Convert.ToByte(@this);
-        }
+        return Convert.ToByte(@this);
     }
 }
