a28530986322fbdb68949eb7b85229621040061e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-17 17:31:11 +0330

    System.TimeSpan

---
+++ Core/System.TimeSpan/TimeSpan.UtcAgo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static partial class Extension
+{
+    /// <summary>
+    ///     A TimeSpan extension method that subtract the specified TimeSpan to the current UTC (Coordinated Universal
+    ///     Time)
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <returns>The current UTC (Coordinated Universal Time) with the specified TimeSpan subtracted from it.</returns>
+    public static DateTime UtcAgo(this TimeSpan @this)
+    {
+        return DateTime.UtcNow.Subtract(@this);
+    }
+}
