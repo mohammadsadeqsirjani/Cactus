5a350d05829b4cd9eae259448ce0e18abd729ec0
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-24 17:36:40 +0330

    Create DateTimeOffSet.EndOfDay.cs

---
+++ Core/System.DateTimeOffset/DateTimeOffSet.EndOfDay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static partial class Extension
+{
+    /// <summary>
+    ///     A DateTimeOffSet extension method that return a DateTimeOffSet with the time set to "23:59:59:999". The last moment of
+    ///     the day. Use "DateTime2" column type in sql to keep the precision.
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <returns>A DateTimeOffSet of the day with the time set to "23:59:59:999".</returns>
+    public static DateTimeOffset EndOfDay(this DateTimeOffset @this)
+    {
+        return new DateTimeOffset(@this.Year, @this.Month, @this.Day, @this.Hour, @this.Minute, @this.Second, @this.Offset)
+            .AddDays(1)
+            .Subtract(new TimeSpan(0, 0, 0, 0, 1));
+    }
+}
