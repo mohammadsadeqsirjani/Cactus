e657f2835833b6cd89e5ed7e481db0d36458711c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-24 17:40:11 +0330

    Update DateTime.FirstDayOfWeek.cs

--- Core/System.DateTimeOffset/DateTime.FirstDayOfWeek.cs
+++ Core/System.DateTimeOffset/DateTime.FirstDayOfWeek.cs
@@ -1,12 +1,13 @@
 ﻿public static partial class Extension
 {
     /// <summary>
-    ///     A DateTime extension method that first day of week.
+    ///     A DateTimeOffset extension method that first day of week.
     /// </summary>
     /// <param name="this">The @this to act on.</param>
     /// <returns>A DateTime.</returns>
-    public static System.DateTime FirstDayOfWeek(this System.DateTime @this)
+    public static System.DateTimeOffset FirstDayOfWeek(this System.DateTimeOffset @this)
     {
-        return new System.DateTime(@this.Year, @this.Month, @this.Day).AddDays(-(int)@this.DayOfWeek);
+        return new System.DateTimeOffset(@this.Year, @this.Month, @this.Day, 0, 0, 0, @this.Offset)
+            .AddDays(-(int)@this.DayOfWeek);
     }
 }
