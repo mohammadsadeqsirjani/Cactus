e2e3c53c77aeed20e80198e5dccfa9bc9e3efd2f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-24 16:23:54 +0330

    Create TimeSpan.Since.cs

---
+++ Core/System.TimeSpan/TimeSpan.Since.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public static partial class Extension
+{
+    /// <summary>
+    ///     A TimeSpan extension method that add the specified TimeSpan to the current DateTime.
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <returns>The current DateTime with the specified TimeSpan added to it.</returns>
+    public static DateTime Since(this TimeSpan @this)
+    {
+        return DateTime.Now.Add(@this);
+    }
+
+    /// <summary>
+    /// Adds given <see cref="TimeSpan"/> to supplied <paramref name="originalValue"/> <see cref="DateTime"/> and returns resulting <see cref="DateTime"/> in the future.
+    /// </summary>
+    public static DateTime Since(this TimeSpan @this, DateTime originalValue)
+    {
+        return originalValue + @this;
+    }
+}
