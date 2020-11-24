69da56d943bf6d54a79e80e91addc0116d3f7e8c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-24 17:40:14 +0330

    Update DateTime.IsAfternoon.cs

--- Core/System.DateTimeOffset/DateTime.IsAfternoon.cs
+++ Core/System.DateTimeOffset/DateTime.IsAfternoon.cs
@@ -1,12 +1,12 @@
 ﻿public static partial class Extension
 {
     /// <summary>
-    ///     A DateTime extension method that query if '@this' is afternoon.
+    ///     A DateTimeOffset extension method that query if '@this' is afternoon.
     /// </summary>
     /// <param name="this">The @this to act on.</param>
     /// <returns>true if afternoon, false if not.</returns>
-    public static bool IsAfternoon(this System.DateTime @this)
+    public static bool IsAfternoon(this System.DateTimeOffset @this)
     {
-        return @this.TimeOfDay >= new System.DateTime(2000, 1, 1, 12, 0, 0).TimeOfDay;
+        return @this.TimeOfDay >= new System.DateTimeOffset(2000, 1, 1, 12, 0, 0, @this.Offset).TimeOfDay;
     }
 }
