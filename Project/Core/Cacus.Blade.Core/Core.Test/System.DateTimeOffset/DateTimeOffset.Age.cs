72b79c75daaaf24af31c8b393f42f97b02f3d9ec
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 13:17:35 +0330

    Create DateTimeOffset.Age.cs

---
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.DateTimeOffset/DateTimeOffset.Age.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Core.Test.System.DateTimeOffset
+{
+    [TestClass]
+    public class DateTimeOffsetAge
+    {
+        [TestMethod]
+        public void Age()
+        {
+            var @this = new global::System.DateTimeOffset(1981, 01, 01, 0, 0, 0, TimeSpan.Zero);
+
+            var result = @this.Age();
+
+            Assert.AreEqual(global::System.DateTimeOffset.Now.Year - 1981, result);
+        }
+    }
+}
