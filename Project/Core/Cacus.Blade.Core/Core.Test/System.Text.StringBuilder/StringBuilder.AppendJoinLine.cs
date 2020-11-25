cfa8a1ab3aa2e5155a2901ee2a04427d4d90432a
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 18:11:41 +0330

    Create StringBuilder.AppendJoinLine.cs

---
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.Text.StringBuilder/StringBuilder.AppendJoinLine.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Core.Test.System.Text.StringBuilder
+{
+    [TestClass]
+    public class SystemTextStringBuilderAppendLineJoin
+    {
+        [TestMethod]
+        public void AppendLineFormat()
+        {
+            var list = new List<string> { "Fizz", "Buzz" };
+
+            var @this = new global::System.Text.StringBuilder();
+
+            @this.AppendLineJoin(",", list);
+
+            Assert.AreEqual("Fizz,Buzz" + "\n", @this.ToString());
+        }
+    }
+}
