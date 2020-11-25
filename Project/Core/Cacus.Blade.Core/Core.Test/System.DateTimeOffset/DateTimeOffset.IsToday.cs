4b4d27b1da89a6067156801493c0e330b1f616b5
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 13:42:00 +0330

    Update DateTimeOffset.IsToday.cs

--- Project/Core/Cacus.Blade.Core/Core.Test/System.DateTimeOffset/DateTimeOffset.IsToday.cs
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.DateTimeOffset/DateTimeOffset.IsToday.cs
@@ -8,11 +8,11 @@ namespace Core.Test.System.DateTimeOffset
         [TestMethod]
         public void IsToday()
         {
-            var @thisToday = global::System.DateTime.Today;
-            var @thisYesterday = @thisToday.AddDays(-1);
+            var thisToday = global::System.DateTime.Today;
+            var thisYesterday = thisToday.AddDays(-1);
 
-            var result1 = @thisToday.IsToday();
-            var result2 = @thisYesterday.IsToday();
+            var result1 = thisToday.IsToday();
+            var result2 = thisYesterday.IsToday();
 
             Assert.IsTrue(result1);
             Assert.IsFalse(result2);
