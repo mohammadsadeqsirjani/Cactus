135d27053ae2f5f93cb182d979d77f05252f6647
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 11:13:58 +0330

    Create IDictionary.GetOrAdd.cs

---
+++ Project/Collection/Cactus.Blade.Collection/Collection.Test/System.Collections.Generic.IDictionary/IDictionary.GetOrAdd.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Collection.Test.System.Collections.Generic.IDictionary
+{
+    [TestClass]
+    public class DictionaryGetOrAdd
+    {
+        [TestMethod]
+        public void GetOrAdd()
+        {
+            var @this = new Dictionary<string, string>();
+
+            string value1 = @this.GetOrAdd("Fizz", "Buzz");
+            string value2 = @this.GetOrAdd("Fizz", "Buzz2");
+            string value3 = @this.GetOrAdd("Fizz2", s => "Buzz");
+
+            Assert.AreEqual("Buzz", value1);
+            Assert.AreEqual("Buzz", value2);
+            Assert.AreEqual("Buzz", value3);
+        }
+    }
+}
