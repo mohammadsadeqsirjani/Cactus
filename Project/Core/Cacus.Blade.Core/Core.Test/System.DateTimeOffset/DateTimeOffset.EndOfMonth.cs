70f0a6b5ce58e72551cf313e40d56a811e8c3006
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 13:18:22 +0330

    Create DateTimeOffset.EndOfMonth.cs

---
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.DateTimeOffset/DateTimeOffset.EndOfMonth.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Core.Test.System.DateTimeOffset
+{
+    [TestClass]
+    public class SystemDateTimeOffsetEndOfMonth
+    {
+        [TestMethod]
+        public void EndOfMonth()
+        {
+            var @this = new global::System.DateTimeOffset(2013, 12, 22, 23, 59, 59, TimeSpan.Zero);
+
+            var value = @this.EndOfMonth();
+
+            Assert.AreEqual(new global::System.DateTimeOffset(2013, 12, 31, 23, 59, 59, TimeSpan.Zero), value);
+        }
+    }
+}
