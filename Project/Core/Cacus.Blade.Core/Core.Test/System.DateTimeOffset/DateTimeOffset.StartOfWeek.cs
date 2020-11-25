6d9eb8b4a77f7ac6f06ee7cd94b401b021650c88
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 13:17:42 +0330

    Create DateTimeOffset.StartOfWeek.cs

---
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.DateTimeOffset/DateTimeOffset.StartOfWeek.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Core.Test.System.DateTimeOffset
+{
+    [TestClass]
+    public class SystemDateTimeOffsetStartOfWeek
+    {
+        [TestMethod]
+        public void StartOfWeek()
+        {
+            var @this = new global::System.DateTimeOffset(2014, 04, 16, 0, 0, 0, TimeSpan.Zero);
+
+            var value = @this.StartOfWeek();
+
+            Assert.AreEqual(new global::System.DateTimeOffset(2014, 04, 13, 0, 0, 0, TimeSpan.Zero), value);
+        }
+    }
+}
