d4b338817ce1b8a42e96b5e06cfed32a03427861
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 13:59:35 +0330

    Create Enum.GetCustomAttributeDescription.cs

---
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.Enum/Enum.GetCustomAttributeDescription.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Test.System.Enum
+{
+    [TestClass]
+    public class SystemEnumGetCustomAttributeDescription
+    {
+
+
+
+        [TestMethod]
+        public void GetCustomAttributeDescription()
+        {
+            const TestEnum @this = TestEnum.Test;
+
+            var result = @this.GetCustomAttributeDescription();
+
+            Assert.AreNotEqual("Test Description", result);
+        }
+    }
+
+
+    public enum TestEnum
+    {
+        Test
+    }
+}
