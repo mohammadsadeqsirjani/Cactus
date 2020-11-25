c376ce03ce119acfeb15081a152a9f3b54474726
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 11:31:19 +0330

    Update IEnumerable.ContainsAny.cs

--- Project/Collection/Cactus.Blade.Collection/Collection.Test/System.Collections.Generic.IEnumerable/IEnumerable.ContainsAny.cs
+++ Project/Collection/Cactus.Blade.Collection/Collection.Test/System.Collections.Generic.IEnumerable/IEnumerable.ContainsAny.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Collection.Test.System.Collections.Generic.IEnumerable
 {
-    class IEnumerable
+    [TestClass]
+    public class EnumerableContainsAny
     {
+        [TestMethod]
+        public void ContainsAny()
+        {
+            var @this = new List<string> { "zA", "zB", "C" }.AsEnumerable();
+
+            var value1 = @this.ContainsAny("1", "zA", "3");
+            var value2 = @this.ContainsAny("1", "2", "3");
+
+            Assert.IsTrue(value1);
+            Assert.IsFalse(value2);
+        }
     }
 }
