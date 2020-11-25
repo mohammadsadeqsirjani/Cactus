d9df7cfba2cb20a53ae7fceca30cc20a25f18958
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 13:42:05 +0330

    Update DateTimeOffset.IsWeekendDay.cs

--- Project/Core/Cacus.Blade.Core/Core.Test/System.DateTimeOffset/DateTimeOffset.IsWeekendDay.cs
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.DateTimeOffset/DateTimeOffset.IsWeekendDay.cs
@@ -9,11 +9,11 @@ namespace Core.Test.System.DateTimeOffset
         [TestMethod]
         public void IsWeekendDay()
         {
-            var @thisFriday = new global::System.DateTimeOffset(2013, 11, 22, 0, 0, 0, TimeSpan.Zero);
-            var @thisSaturday = new global::System.DateTimeOffset(2013, 11, 23, 0, 0, 0, TimeSpan.Zero);
+            var thisFriday = new global::System.DateTimeOffset(2013, 11, 22, 0, 0, 0, TimeSpan.Zero);
+            var thisSaturday = new global::System.DateTimeOffset(2013, 11, 23, 0, 0, 0, TimeSpan.Zero);
 
-            var value1 = @thisFriday.IsWeekendDay();
-            var value2 = @thisSaturday.IsWeekendDay();
+            var value1 = thisFriday.IsWeekendDay();
+            var value2 = thisSaturday.IsWeekendDay();
 
             Assert.IsFalse(value1);
             Assert.IsTrue(value2);
