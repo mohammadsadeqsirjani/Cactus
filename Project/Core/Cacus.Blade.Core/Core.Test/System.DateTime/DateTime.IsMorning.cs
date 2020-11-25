b046a8ade1e21e814e4c73df848d6fe667c2d83e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 13:41:35 +0330

    Update DateTime.IsMorning.cs

--- Project/Core/Cacus.Blade.Core/Core.Test/System.DateTime/DateTime.IsMorning.cs
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.DateTime/DateTime.IsMorning.cs
@@ -8,11 +8,11 @@ namespace Core.Test.System.DateTime
         [TestMethod]
         public void IsMorning()
         {
-            var @thisMorning = new global::System.DateTime(2014, 04, 12, 8, 0, 0);
-            var @thisAfternoon = new global::System.DateTime(2014, 04, 12, 17, 0, 0);
+            var thisMorning = new global::System.DateTime(2014, 04, 12, 8, 0, 0);
+            var thisAfternoon = new global::System.DateTime(2014, 04, 12, 17, 0, 0);
 
-            var result1 = @thisMorning.IsMorning();
-            var result2 = @thisAfternoon.IsMorning();
+            var result1 = thisMorning.IsMorning();
+            var result2 = thisAfternoon.IsMorning();
 
             Assert.IsTrue(result1);
             Assert.IsFalse(result2);
