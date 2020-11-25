4516f09772bb747327d7a4fa699687c34c7f3c96
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 18:11:46 +0330

    Create StringBuilder.AppendLineIf.cs

---
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.Text.StringBuilder/StringBuilder.AppendLineIf.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Test.System.Text.StringBuilder
+{
+    [TestClass]
+    public class SystemTextStringBuilderAppendLineIf
+    {
+        [TestMethod]
+        public void AppendLineIf()
+        {
+            var @this = new global::System.Text.StringBuilder();
+
+            @this.AppendLineIf(x => x.Contains("F"), "Fizz", "Buzz");
+
+            Assert.AreEqual("Fizz" + "\n", @this.ToString());
+        }
+    }
+}
