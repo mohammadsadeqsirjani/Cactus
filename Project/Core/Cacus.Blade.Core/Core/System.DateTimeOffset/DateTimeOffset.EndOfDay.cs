f7a4d3f6d7b093bc91187c89e840f9bdab634eff
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 17:19:40 +0330

    Update DateTimeOffset.EndOfDay.cs

--- Project/Core/Cacus.Blade.Core/Core/System.DateTimeOffset/DateTimeOffset.EndOfDay.cs
+++ Project/Core/Cacus.Blade.Core/Core/System.DateTimeOffset/DateTimeOffset.EndOfDay.cs
@@ -10,8 +10,6 @@ public static partial class Extension
     /// <returns>A DateTimeOffSet of the day with the time set to "23:59:59:999".</returns>
     public static DateTimeOffset EndOfDay(this DateTimeOffset @this)
     {
-        return new DateTimeOffset(@this.Year, @this.Month, @this.Day, @this.Hour, @this.Minute, @this.Second, @this.Offset)
-            .AddDays(1)
-            .Subtract(new TimeSpan(0, 0, 0, 0, 1));
+        return new DateTimeOffset(@this.Year, @this.Month, @this.Day, 23, 59, 59, @this.Offset);
     }
 }
