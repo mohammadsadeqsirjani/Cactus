2d3c4d75ea93f0d676ddcb12818fcca3a02e1456
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 13:59:43 +0330

    Create Enum.NotIn.cs

---
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.Enum/Enum.NotIn.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Core.Test.System.Enum
+{
+    [TestClass]
+    public class SystemEnumNotIn
+    {
+        [TestMethod]
+        public void NotIn()
+        {
+            const Environment.SpecialFolder @this = Environment.SpecialFolder.Desktop;
+
+            var result1 = @this.NotIn(Environment.SpecialFolder.Desktop, Environment.SpecialFolder.DesktopDirectory);
+            var result2 = @this.NotIn(Environment.SpecialFolder.DesktopDirectory);
+
+            Assert.IsFalse(result1);
+            Assert.IsTrue(result2);
+        }
+    }
+}
