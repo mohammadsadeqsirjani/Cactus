30a93800d30a1a63164c4f9230b74e65a90af7a2
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 13:41:45 +0330

    Update DateTime.IsWeekendDay.cs

--- Project/Core/Cacus.Blade.Core/Core.Test/System.DateTime/DateTime.IsWeekendDay.cs
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.DateTime/DateTime.IsWeekendDay.cs
@@ -8,11 +8,11 @@ namespace Core.Test.System.DateTime
         [TestMethod]
         public void IsWeekendDay()
         {
-            var @thisFriday = new global::System.DateTime(2013, 11, 22);
-            var @thisSaturday = new global::System.DateTime(2013, 11, 23);
+            var thisFriday = new global::System.DateTime(2013, 11, 22);
+            var thisSaturday = new global::System.DateTime(2013, 11, 23);
 
-            var value1 = @thisFriday.IsWeekendDay();
-            var value2 = @thisSaturday.IsWeekendDay();
+            var value1 = thisFriday.IsWeekendDay();
+            var value2 = thisSaturday.IsWeekendDay();
 
             Assert.IsFalse(value1);
             Assert.IsTrue(value2);
