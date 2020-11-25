60234021684397871140184f93c4ad09965096a8
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 12:53:28 +0330

    Create DateTime.StartOfMonth.cs

---
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.DateTime/DateTime.StartOfMonth.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Test.System.DateTime
+{
+    [TestClass]
+    public class SystemDateTimeStartOfMonth
+    {
+        [TestMethod]
+        public void StartOfMonth()
+        {
+            global::System.DateTime @this = global::System.DateTime.Now;
+
+            global::System.DateTime value = @this.StartOfMonth();
+
+            Assert.AreEqual(new global::System.DateTime(value.Year, value.Month, 1), value);
+        }
+    }
+}
