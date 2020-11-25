5b68da038b1134e9bffb04432529fda9ae8d51d0
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 18:29:47 +0330

    Update StringBuilder.ExtractCommentSingleLine.cs

--- Project/Core/Cacus.Blade.Core/Core.Test/System.Text.StringBuilder/StringBuilder.ExtractCommentSingleLine.cs
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.Text.StringBuilder/StringBuilder.ExtractCommentSingleLine.cs
@@ -11,7 +11,7 @@ namespace Core.Test.System.Text.StringBuilder
             Assert.AreEqual(null, new global::System.Text.StringBuilder(" ").ExtractCommentSingleLine());
             Assert.AreEqual("//z", new global::System.Text.StringBuilder("//z").ExtractCommentSingleLine().ToString());
             Assert.AreEqual("//z", new global::System.Text.StringBuilder("////z").ExtractCommentSingleLine(2).ToString());
-            Assert.AreEqual("//z", new global::System.Text.StringBuilder("////z" + "\n" + "z").ExtractCommentSingleLine(2, out var endIndex).ToString());
+            Assert.AreEqual("//z", new global::System.Text.StringBuilder("////z" + global::System.Environment.NewLine + "z").ExtractCommentSingleLine(2, out var endIndex).ToString());
             Assert.AreEqual(5, endIndex);
         }
     }
