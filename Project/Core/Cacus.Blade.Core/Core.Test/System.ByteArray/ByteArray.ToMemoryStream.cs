4caa02ec1795898fb676b2c63469d315402a3a0d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 12:28:45 +0330

    Update ByteArray.ToMemoryStream.cs

--- Project/Core/Cacus.Blade.Core/Core.Test/System.ByteArray/ByteArray.ToMemoryStream.cs
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.ByteArray/ByteArray.ToMemoryStream.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.IO;
 
 namespace Core.Test.System.ByteArray
 {
@@ -11,7 +10,7 @@ namespace Core.Test.System.ByteArray
         {
             var @this = new byte[1] { 1 };
 
-            MemoryStream result = @this.ToMemoryStream();
+            var result = @this.ToMemoryStream();
 
             Assert.AreEqual(1, result.ToArray()[0]);
         }
