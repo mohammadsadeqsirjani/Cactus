49f13d7c8c80bbcd485c2b527bdca839fcc99ee2
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 13:41:32 +0330

    Update DateTime.IsDateEqual.cs

--- Project/Core/Cacus.Blade.Core/Core.Test/System.DateTime/DateTime.IsDateEqual.cs
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.DateTime/DateTime.IsDateEqual.cs
@@ -8,10 +8,10 @@ namespace Core.Test.System.DateTime
         [TestMethod]
         public void IsDateEqual()
         {
-            var @thisMorning = new global::System.DateTime(2014, 04, 12, 8, 0, 0);
-            var @thisAfternoon = new global::System.DateTime(2014, 04, 12, 17, 0, 0);
+            var thisMorning = new global::System.DateTime(2014, 04, 12, 8, 0, 0);
+            var thisAfternoon = new global::System.DateTime(2014, 04, 12, 17, 0, 0);
 
-            var result = @thisMorning.IsDateEqual(@thisAfternoon);
+            var result = thisMorning.IsDateEqual(thisAfternoon);
 
             Assert.IsTrue(result);
         }
