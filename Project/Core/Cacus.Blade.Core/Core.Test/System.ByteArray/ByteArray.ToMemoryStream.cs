5807844aa6fc3bc390e6aaeb19e544785c80b0b8
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 12:18:31 +0330

    Create ByteArray.ToMemoryStream.cs

---
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.ByteArray/ByteArray.ToMemoryStream.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Core.Test.System.ByteArray
+{
+    [TestClass]
+    public class ByteArrayToMemoryStream
+    {
+        [TestMethod]
+        public void ToMemoryStream()
+        {
+            var @this = new byte[1] { 1 };
+
+            MemoryStream result = @this.ToMemoryStream();
+
+            Assert.AreEqual(1, result.ToArray()[0]);
+        }
+    }
+}
