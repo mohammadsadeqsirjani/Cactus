0388d91e1edf57358f65dabf5c72167273ef7d65
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-11-25 13:59:40 +0330

    Create Enum.In.cs

---
+++ Project/Core/Cacus.Blade.Core/Core.Test/System.Enum/Enum.In.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Core.Test.System.Enum
+{
+    [TestClass]
+    public class SystemEnumIn
+    {
+        [TestMethod]
+        public void In()
+        {
+            const Environment.SpecialFolder @this = Environment.SpecialFolder.Desktop;
+
+            var result1 = @this.In(Environment.SpecialFolder.Desktop, Environment.SpecialFolder.DesktopDirectory);
+            var result2 = @this.In(Environment.SpecialFolder.DesktopDirectory);
+
+            Assert.IsTrue(result1);
+            Assert.IsFalse(result2);
+        }
+    }
+}
