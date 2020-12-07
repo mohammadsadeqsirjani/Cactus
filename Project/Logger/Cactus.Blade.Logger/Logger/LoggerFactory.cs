fa3c0463c657ca8d744255035d7b632447b1c2b6
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-07 19:57:25 +0330

    Create LoggerFactory.cs

---
+++ Project/Logger/Cactus.Blade.Logger/Logger/LoggerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cactus.Blade.Logger
+{
+    /// <summary>
+    /// A factory for creating <see cref="ILogger"/> instances using <typeparamref name="T"/> as the name of the logger.
+    /// </summary>
+    /// <typeparam name="T">The type used to name the <see cref="ILogger"/>.</typeparam>
+    public class LoggerFactory<T> : ILoggerFactory<T>
+    {
+        // lazy singleton of logger
+        private static readonly Lazy<ILogger> Logger = new Lazy<ILogger>(Blade.Logger.Logger.CreateLogger<T>);
+
+        /// <summary>
+        /// Create an instance of <see cref="ILogger" /> using <typeparamref name="T" /> as the logger name.
+        /// </summary>
+        /// <returns>
+        /// An instance of <see cref="ILogger" />.
+        /// </returns>
+        public ILogger CreateLogger()
+        {
+            return Logger.Value;
+        }
+    }
+}
