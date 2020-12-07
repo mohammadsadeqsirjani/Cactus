dd61991a1af90554ef04839597cdc7bc0e03bb5d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-07 19:57:05 +0330

    Create ILoggerFactory.cs

---
+++ Project/Logger/Cactus.Blade.Logger/Logger/ILoggerFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Cactus.Blade.Logger
+{
+    /// <summary>
+    /// A factory for creating <see cref="ILogger"/> instances using <typeparamref name="T"/> as the name of the logger.
+    /// </summary>
+    /// <typeparam name="T">The type used to name the <see cref="ILogger"/>.</typeparam>
+    public interface ILoggerFactory<T>
+    {
+        /// <summary>
+        /// Create an instance of <see cref="ILogger"/> using <typeparamref name="T"/> as the logger name.
+        /// </summary>
+        /// <returns>An instance of <see cref="ILogger"/>.</returns>
+        ILogger CreateLogger();
+    }
+}
