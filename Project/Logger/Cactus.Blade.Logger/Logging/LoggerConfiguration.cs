e9b622bdcd5c00875b1e509a33c5be65f1547422
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-08 18:59:32 +0330

    Create LoggerConfiguration.cs

---
+++ Project/Logger/Cactus.Blade.Logger/Logging/LoggerConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Logging
+{
+    public class LoggerConfiguration : ILoggerConfiguration
+    {
+        public bool IsLoggingEnabled { get; set; }
+        public LogLevel LoggingLevel { get; set; }
+    }
+}
