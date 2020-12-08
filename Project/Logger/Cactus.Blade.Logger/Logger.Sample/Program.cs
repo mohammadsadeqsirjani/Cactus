a33dbf6e361f80ee13188b300494207625f4cfab
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-08 17:19:43 +0330

    Update Program.cs

--- Project/Logger/Cactus.Blade.Logger/Logger.Sample/Program.cs
+++ Project/Logger/Cactus.Blade.Logger/Logger.Sample/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Logger.Sample
 {
@@ -6,7 +7,14 @@ namespace Logger.Sample
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
