9c147b82d1d504d0b3be32747085b9a172484c66
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-08 18:36:30 +0330

    Create IAsyncWaitHandle.cs

---
+++ Project/Logger/Cactus.Blade.Logger/Logging/IAsyncWaitHandle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    public interface IAsyncWaitHandle : IDisposable
+    {
+        Task WaitAsync(
+            int millisecondsTimeout = Timeout.Infinite,
+            CancellationToken cancellationToken = default);
+
+        void Release(int releaseCount = 1);
+    }
+}
