a164199babcf369f6c692b64d40714bd7b0f5cb3
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-08 18:36:37 +0330

    Create ContextProvider.cs

---
+++ Project/Logger/Cactus.Blade.Logger/Logging/ContextProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace Logging
+{
+    public abstract class ContextProvider<TLogEntry> : IContextProvider
+        where TLogEntry : ILogEntry
+    {
+        public void AddContextData(ILogEntry logEntry)
+        {
+            if (logEntry is TLogEntry)
+            {
+                var tLogEntry = (TLogEntry)logEntry;
+                AddContextData(tLogEntry);
+            }
+        }
+
+        protected abstract void AddContextData(TLogEntry logEntry);
+    }
+}
