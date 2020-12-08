c27439fb3997e05cef111e9b2730b903a934ce27
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-08 18:36:11 +0330

    Create Category.cs

---
+++ Project/Logger/Cactus.Blade.Logger/Logging/Configuration/Xml/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace Logging.Configuration.Xml
+{
+    [XmlRoot("category")]
+    public class Category
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("ThrottlingRule ")]
+        public string ThrottlingRule { get; set; }
+
+        [XmlArray("providers")]
+        [XmlArrayItem("provider")]
+        public LogProviderProxy[] LogProviders { get; set; }
+    }
+}
