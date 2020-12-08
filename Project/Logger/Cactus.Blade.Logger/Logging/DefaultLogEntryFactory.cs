5877042b51c1f9927286b15bf9f2a696c4d749bc
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-08 18:36:32 +0330

    Create DefaultLogEntryFactory.cs

---
+++ Project/Logger/Cactus.Blade.Logger/Logging/DefaultLogEntryFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace Logging
+{
+    public static class DefaultLogEntryFactory
+    {
+        private static readonly Semimutable<ILogEntryFactory> _logEntryFactory = new Semimutable<ILogEntryFactory>(GetDefault);
+
+        public static ILogEntryFactory Current
+        {
+            get { return _logEntryFactory.Value; }
+        }
+
+        public static void SetCurrent(ILogEntryFactory logEntryFactory)
+        {
+            _logEntryFactory.Value = logEntryFactory;
+        }
+
+        private static ILogEntryFactory GetDefault()
+        {
+            return new LogEntryFactory();
+        }
+    }
+}
