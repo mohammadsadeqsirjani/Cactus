5cf7b5261dfc4b23b1c27ba0ae97306ae8ad6cc3
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-08 18:36:28 +0330

    Create IContextProvider.cs

---
+++ Project/Logger/Cactus.Blade.Logger/Logging/IContextProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logging
+{
+    public interface IContextProvider
+    {
+        void AddContextData(ILogEntry logEntry);
+    }
+}
